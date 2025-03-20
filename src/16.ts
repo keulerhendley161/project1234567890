8181498d5ec0417f4327547ae9f9b63b5567cdeb
keulerhendley161 <keulerhendley161@gmail.com>
2025-03-20 05:25:47 +0000

    Add 16.ts

---
+++ src/16.ts
@@ -0,0 +1,15 @@
+// Project1234567890
+
+interface Student {
+  name: string;
+  age: number;
+  gpa: number;
+}
+
+const student1: Student = {
+  name: 'John Doe',
+  age: 25,
+  gpa: 3.5,
+};
+
+console.log(student1);
